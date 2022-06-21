c991e70bffe110f2d1713d5b736c4e2b60f7ec41
Obedient-Putro <107755767+Obedient-Putro@users.noreply.github.com>
2022-06-21 23:13:44 +0800

    Update database.js

--- api/config/database.js
+++ api/config/database.js
@@ -1,6 +1,6 @@
 import sequelize from "sequelize";
 
-export const db = new sequelize('sql6500598', 'sql6500598', 'Ve5f2K8RZS', {
-    host: "sql6.freemysqlhosting.net",
+export const db = new sequelize('scale_db', 'root', 'learndb', {
+    host: "localhost",
     dialect: "mysql"
 });
